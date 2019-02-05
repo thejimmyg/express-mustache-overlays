527002fca18103353522cb380906db7ae21d87eb
James Gardner <james@jimmyg.org>
2019-02-05 19:01:25 +0000

    Refactor for prune 2

--- bin/server.js
+++ bin/server.js
@@ -1,42 +1,19 @@
+// DEMO_ROUTES=true MUSTACHE_DIRS=overlay DEBUG=express-mustache-overlays,express-mustache-overlays:server PORT=8000 DEFAULT_TITLE=Hello npm start
+
+const { expressMustacheOverlays } = require('../index.js')
 const express = require('express')
-const compression = require('compression')
-const { overlaysApp, overlaysOptionsFromEnv, overlaysDirsFromEnv, prepareMustacheOverlays, setupErrorHandlers } = require('../lib/index.js')
-const debug = require('debug')('express-mustache-overlays:server')
-// const publicFilesDir = path.normalize(path.join(__dirname, '..', 'public'))
-const demoRoutes = (process.env.DEMO_ROUTES || 'false').toLowerCase() === 'true'
 const mustache = require('mustache')
+const debug = require('debug')('express-mustache-overlays:server')
 
-const main = async () => {
-  const app = express()
-  app.use(compression())
+const app = express()
+app.locals.demoRoutes = (process.env.DEMO_ROUTES || 'false').toLowerCase() === 'true'
+app.locals.debug = debug
 
-  const port = process.env.PORT || 80
+expressMustacheOverlays(app, {}, () => {
+  const { scriptName } = app.locals
 
-  const overlaysOptions = overlaysOptionsFromEnv()
-  // Extract the options you want to use elsewhere in your script too
-  const { scriptName } = overlaysOptions
-  const { mustacheDirs, publicFilesDirs } = overlaysDirsFromEnv()
-
-  overlaysOptions.expressStaticOptions = {}
-
-  const overlays = await prepareMustacheOverlays(app, overlaysOptions)
-
-  mustacheDirs.forEach(dir => {
-    overlays.overlayMustacheDir(dir)
-  })
-  publicFilesDirs.forEach(dir => {
-    overlays.overlayPublicFilesDir(dir)
-  })
-
-  app.use((req, res, next) => {
-    res.locals = Object.assign({}, res.locals, { demoRoutes })
-    next()
-  })
-
-  const oApp = await overlaysApp(overlays, { debug })
-
-  if (demoRoutes) {
-    oApp.get('/throw', async (req, res, next) => {
+  if (app.locals.demoRoutes) {
+    app.get(scriptName + '/throw', async (req, res, next) => {
       try {
         throw new Error('Sample error')
       } catch (e) {
@@ -45,9 +22,9 @@ const main = async () => {
     })
 
     // Render the page, with the default title and request username as well as the content
-    oApp.get('/', async (req, res, next) => {
+    app.get(scriptName + '/', async (req, res, next) => {
       try {
-        const html = await overlays.renderView('content', { content: 'render()', metaDescription: 'Home page' })
+        const html = await (await app.locals.overlaysPromise).renderView('content', { content: 'render()', metaDescription: 'Home page' })
         res.render('content', { content: '<h1>Home</h1><p>Hello!</p><pre>' + mustache.escape(html) + '</pre>', metaDescription: 'Home page' })
       } catch (e) {
         next(e)
@@ -55,7 +32,7 @@ const main = async () => {
     })
 
     // Another page
-    oApp.get('/ok', async (req, res, next) => {
+    app.get(scriptName + '/ok', async (req, res, next) => {
       try {
         res.render('content', { content: '<h1>OK</h1><p>OK!</p>', metaDescription: 'OK page' })
       } catch (e) {
@@ -63,25 +40,6 @@ const main = async () => {
       }
     })
   }
-  debug('Mounting overlays app at', scriptName)
-  app.use(scriptName, oApp)
 
-  // Put the overlays into place after you've set up any more overlays you need, but definitely before the error handlers
-  await overlays.setup({ debug })
-  // Keep this right at the end, immediately before listening
-  setupErrorHandlers(app, { debug })
-  app.listen(port, () => console.log(`Example app listening on port ${port}`))
-}
-
-main()
-
-// Better handling of SIGINT and SIGTERM for docker
-process.on('SIGINT', function () {
-  console.log('Received SIGINT. Exiting ...')
-  process.exit()
-})
-
-process.on('SIGTERM', function () {
-  console.log('Received SIGTERM. Exiting ...')
-  process.exit()
+  app.listen(app.locals.port, () => console.log(`Example app listening on port ${app.locals.port}`))
 })
